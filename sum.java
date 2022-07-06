cf66a0b9772da7a2909019e1b58978ac292104c1
Vivek Malla <thisisvivekmalla@gmail.com>
2022-07-06 11:19:58 +0530

    sum again changed

--- sum.java
+++ sum.java
@@ -2,7 +2,8 @@ public class sum {
         public static void main(String[] args) {
             int a=2;
             int b=3;
-            int c = a+b;
+            int d =7;
+            int c = a+b+d;
             System.out.println(c);
         }
 }
